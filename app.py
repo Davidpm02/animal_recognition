21b9355aba634a4834c757e0fca78a1f746ceac0
David Padilla <109883212+Davidpm02@users.noreply.github.com>
2023-11-05 04:47:15 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask,redirect,url_for,render_template,request
+from flask import Flask, redirect, url_for, render_template, request, jsonify
 import os
 import cv2
 import numpy as np
+from PIL import Image, ImageFile
+from io import BytesIO
 
 import tensorflow as tf
 
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
 app=Flask(__name__)
+
+UPLOAD_FOLDER = 'imagenesUsuario/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 ####################################################################################
 
@@ -56,26 +63,29 @@ def obtenerPrediccionModelo(modeloCNN, rutaImagen):
                               Este valor numerico corresponde a una etiqueta en cuestion, que se procesara mas adelante.
     """
     
+    try:
     
-    
-    img = cv2.imread(rutaImagen, cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, (120, 160))
-    
-    # Expandir las dimensiones de la imagen para que coincidan con las expectativas del modelo
-    img = np.expand_dims(img, axis=-1)
+        img = cv2.imread(rutaImagen, cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (120, 160))
+        
+        # Expandir las dimensiones de la imagen para que coincidan con las expectativas del modelo
+        img = np.expand_dims(img, axis=-1)
 
-    img = img / 255
-    
-    predictions = modeloCNN.predict(np.expand_dims(img, axis=0))
+        img = img / 255
+        
+        predictions = modeloCNN.predict(np.expand_dims(img, axis=0))
 
-    # Obtiene la clase predicha (la clase con la probabilidad más alta)
-    clase_predicha = np.argmax(predictions, axis=1)
+        # Obtiene la clase predicha (la clase con la probabilidad más alta)
+        clase_predicha = np.argmax(predictions, axis=1)
 
-    # Obtiene la probabilidad de la clase predicha
-    #probabilidad_predicha = predictions[0][clase_predicha]
-    
-    return clase_predicha
+        # Obtiene la probabilidad de la clase predicha
+        #probabilidad_predicha = predictions[0][clase_predicha]
+        
+        return clase_predicha
 
+    except Exception as e:
+        
+        print('Ha ocurrido un error al alimentar al modelo con la imagen proporcionada ==>', e)
 
 
 # -----------------------------------------------------------------------------------------
@@ -94,22 +104,59 @@ def generar_hashmapClases():
     """
     
     
+    try:
     
-    
-    claves = list(range(0, 10))  # Creo una lista con numeros del 0 al 9, que corresponden a las salidas que ofrece el modelo entrenado.
-    nombreEtiquetas = ['mariposa', 'gato', 'gallina', 'vaca', 'perro', 'elefante', 'caballo', 'arania', 'oveja', 'ardilla']
-    dictClases = {}
-    
-    for indice, clave in enumerate(claves):
-        dictClases[clave] = nombreEtiquetas[indice]
+        claves = list(range(0, 10))  # Creo una lista con numeros del 0 al 9, que corresponden a las salidas que ofrece el modelo entrenado.
+        nombreEtiquetas = ['mariposa', 'gato', 'gallina', 'vaca', 'perro', 'elefante', 'caballo', 'arania', 'oveja', 'ardilla']
+        dictClases = {}
         
+        for indice, clave in enumerate(claves):
+            dictClases[clave] = nombreEtiquetas[indice]
+            
+        
+        print('Diccionario de mapeo de clases generado con exito.')
+        
+        return dictClases
     
-    print('Diccionario de mapeo de clases generado con exito.')
+    except Exception as e:
+        
+        print('Ha ocurrido un error al generar el hashmap que mapea las etiquetas del modelo ==>', e)
+
+
+
+# -----------------------------------------------------------------------------------------
+
+def guardar_imagen(bytes_imagen, ruta_archivo):
+    try:
+        imagen = Image.open(BytesIO(bytes_imagen))
+        imagen.save(ruta_archivo)
+        return True
+    except Exception as e:
+        print(f'Error al guardar la imagen: {str(e)}')
+        return False
+
     
-    return dictClases
     
+ # -----------------------------------------------------------------------------------------
+
+def vaciarDirectorioImagenes():
     
+    """Funcion sin parametros que elimina todo el contenido dentro del directorio definido.
     
+       Args:
+       
+       
+       Returns:
+       
+    """
+    
+    directorioImagenes = 'imagenesUsuario/'
+    
+    for archivo in os.listdir(directorioImagenes):
+        os.remove(os.path.join(directorioImagenes, archivo))
+        print('El archivo "{}" ha sido eliminado con exito.'.format(archivo))
+    
+       
     
 ####################################################################################
 
@@ -126,29 +173,57 @@ def home():
 
 
 
-@app.route('/clasificarImagen', methods=['GET','POST'])
+@app.route('/clasificarImagen', methods=['GET', 'POST'])
 def clasificarImagen():
-    
-    modeloCNN = cargarModelo()
-    #rutaImagen = os.path.join(...)
-    
-    etiqueta_predicha =  obtenerPrediccionModelo(modeloCNN, rutaImagen)  # valor numerico ==> salida del modelo
-    dictClases = generar_hashmapClases()
-    
-    clase_predicha = dictClases[etiqueta_predicha]  # string ==> nombre etiqueta predicha (especie animal)
-    
-    # .
-    # .
-    # .
-    
-    return redirect('index.html')
-    
-    
-    
-    
-    
-    
+    carpetaDestino = 'imagenesUsuario/'
 
+    if not os.path.exists(carpetaDestino):
+        os.mkdir(carpetaDestino)
+    else:
+        print('El directorio objetivo ya existe.')
+
+    print('Metodo de solicitud ==>', request.method)
+
+    if request.method == 'POST':
+        archivo = request.files['imagen']
+
+        # Verifica si se ha subido un archivo
+        if archivo.filename != '':
+            
+            nombre_archivo = 'imagen_subida.png'  # Nombre del archivo de salida
+            ruta_archivo = os.path.join(app.config['UPLOAD_FOLDER'], nombre_archivo)
+
+            # Guarda el archivo en la carpeta como una imagen
+            if guardar_imagen(archivo.read(), ruta_archivo):
+                
+                modeloCNN = cargarModelo()
+                dictClases = generar_hashmapClases()
+                
+                
+                etiqueta_predicha =  obtenerPrediccionModelo(modeloCNN, ruta_archivo)  # array con valor numerico ==> salida del modelo
+                print('Etiqueta predicha ==> ', etiqueta_predicha)
+                clase_predicha = dictClases[etiqueta_predicha[0]]  # string ==> nombre etiqueta predicha (especie animal)
+                print(clase_predicha)
+                
+                # .
+                # .
+                # .
+
+                # Retorna una respuesta (por ejemplo, la ruta del archivo guardado)
+                return clase_predicha
+            else:
+                return 'Error al procesar la imagen.'
+        else:
+            return 'No se ha subido ningún archivo.'
+
+    #return redirect('index.html')
+    
+    
+    if len(os.listdir('imagenesUsuario/')) > 0:
+        vaciarDirectorioImagenes()
+        
+    else:
+        print('El directorio objetivo ya esta vacio.')
 
 
 
